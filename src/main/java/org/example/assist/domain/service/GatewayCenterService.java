20cc2ea1a7f06ebc4bb10bc9fab7e1a16134393e
Niudfear <hello@itcast.cn>
2024-11-28 15:42:52 +0800

    订阅服务注册消息驱动网关映射

--- src/main/java/org/example/assist/domain/service/GatewayCenterService.java
+++ src/main/java/org/example/assist/domain/service/GatewayCenterService.java
@@ -27,7 +27,7 @@ public class GatewayCenterService {
         paramMap.put("gatewayAddress", gatewayAddress);
         String resultStr;
         try {
-            resultStr = HttpUtil.post(address + "/wg/admin/config/registerGateway", paramMap, 550);
+            resultStr = HttpUtil.post(address + "/wg/admin/config/registerGateway", paramMap, 60000);
         } catch (Exception e) {
             logger.error("网关服务注册异常，链接资源不可用：{}", address + "/wg/admin/config/registerGateway");
             throw e;
@@ -39,14 +39,15 @@ public class GatewayCenterService {
             throw new GatewayException("网关服务注册异常 [gatewayId：" + gatewayId + "] 、[gatewayAddress：" + gatewayAddress + "]");
     }
 
-    public ApplicationSystemRichInfo pullApplicationSystemRichInfo(String address, String gatewayId) {
+    public ApplicationSystemRichInfo pullApplicationSystemRichInfo(String address, String gatewayId, String systemId) {
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("gatewayId", gatewayId);
+        paramMap.put("systemId", systemId);
         String resultStr;
         try {
-            resultStr = HttpUtil.post(address + "/wg/admin/config/queryApplicationSystemRichInfo", paramMap, 550);
+            resultStr = HttpUtil.post(address + "/wg/admin/config/queryApplicationSystemRichInfo", paramMap, 60000);
         } catch (Exception e) {
-            logger.error("网关服务拉取异常，链接资源不可用：{}", address + "/wg/admin/config/queryApplicationSystemRichInfo");
+            logger.error("网关服务拉取异常，链接资源不可用：{}", address + "/wg/admin/config/queryApplicationSystemRichInfo", e);
             throw e;
         }
         Result<ApplicationSystemRichInfo> result = JSON.parseObject(resultStr, new TypeReference<Result<ApplicationSystemRichInfo>>() {
@@ -57,5 +58,22 @@ public class GatewayCenterService {
         return result.getData();
     }
 
+    public Map<String, String> queryRedisConfig(String address) {
+        String resultStr;
+        try {
+            resultStr = HttpUtil.post(address + "/wg/admin/config/queryRedisConfig", "", 60000);
+        } catch (Exception e) {
+            logger.error("网关服务拉取配置异常，链接资源不可用：{}", address + "/wg/admin/config/queryRedisConfig", e);
+            throw e;
+        }
+        Result<Map<String, String>> result = JSON.parseObject(resultStr, new TypeReference<Result<Map<String, String>>>() {
+        });
+        logger.info("从网关中心拉取Redis配置信息完成。result：{}", resultStr);
+        if (!"0000".equals(result.getCode()))
+            throw new GatewayException("从网关中心拉取Redis配置信息异常");
+        return result.getData();
+    }
+
 }
+
 
--- src/test/java/org/example/assist/ApiTest.java
+++ src/test/java/org/example/assist/ApiTest.java
@@ -27,7 +27,7 @@ public class ApiTest {
         paramMap.put("gatewayName", "电商配送网关");
         paramMap.put("gatewayAddress", "127.0.0.1");
 
-        String resultStr = HttpUtil.post("http://localhost:80/wg/admin/config/registerGateway", paramMap, 60000);
+        String resultStr = HttpUtil.post("http://localhost:8001/wg/admin/config/registerGateway", paramMap, 60000);
         System.out.println(resultStr);
 
         Result result = JSON.parseObject(resultStr, Result.class);
@@ -38,12 +38,14 @@ public class ApiTest {
     public void test_pullApplicationSystemRichInfo() {
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("gatewayId", "api-gateway-g4");
-        String resultStr = HttpUtil.post("http://localhost:80/wg/admin/config/queryApplicationSystemRichInfo", paramMap, 60000);
-        Result<ApplicationSystemRichInfo> result = JSON.parseObject(resultStr, new TypeReference<Result<ApplicationSystemRichInfo>>(){});
+        String resultStr = HttpUtil.post("http://127.0.0.1:8001/wg/admin/config/queryApplicationSystemRichInfo", paramMap, 60000);
+        Result<ApplicationSystemRichInfo> result = JSON.parseObject(resultStr, new TypeReference<Result<ApplicationSystemRichInfo>>() {
+        });
         System.out.println(JSON.toJSONString(result.getData()));
     }
 
 }
+
 
 
 
